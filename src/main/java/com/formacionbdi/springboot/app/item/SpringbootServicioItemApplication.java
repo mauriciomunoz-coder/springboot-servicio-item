07a784c79bcbadf264873fee3707280d411e742d
maomauriciomunoz <maomauriciomunoz@gmail.com>
2022-01-19 15:55:02 -0500

    video 17. configuracion de OPenFeign

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,8 +2,10 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringbootServicioItemApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -0,0 +1,21 @@
+package com.formacionbdi.springboot.app.item.clientes;
+
+import com.formacionbdi.springboot.app.item.models.Producto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "servicio-productos", url = "localhost:8001")  //nombre y puerto del microservicio a consumir
+public interface ProductoClienteRest {
+
+    //metodos traidos del controlador del microservicio service-productos
+
+    @GetMapping("/listar")
+    public List<Producto> listar();
+
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable Long id);
+}
