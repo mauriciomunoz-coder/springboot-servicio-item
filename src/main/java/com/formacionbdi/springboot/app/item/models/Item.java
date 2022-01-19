9bf2bfc45524b254113c86a9fc2d0ea455f13d41
maomauriciomunoz <maomauriciomunoz@gmail.com>
2022-01-19 14:25:19 -0500

    video 13. creacion del proyecto springboot-servicio-items : creamos las clases item y producto

---
+++ src/main/java/com/formacionbdi/springboot/app/item/models/Item.java
@@ -0,0 +1,20 @@
+package com.formacionbdi.springboot.app.item.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    private Producto producto;
+    private Integer cantidad;
+
+
+    public Double getTotal() {
+        return producto.getPrecio() * cantidad;
+    }
+}
---
+++ src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -0,0 +1,20 @@
+package com.formacionbdi.springboot.app.item.models;
+
+import lombok.Data;
+
+import java.util.Date;
+
+
+@Data
+public class Producto {
+
+
+    private Long id;
+    private String nombre;
+    private Double precio;
+
+
+    private Date createAt;
+
+
+}
