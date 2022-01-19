c377ecf69c988757853be205743d8c481582ec92
maomauriciomunoz <maomauriciomunoz@gmail.com>
2022-01-19 15:03:20 -0500

    video 15. insertamos el codigo en ItemServiceImpl para configurar el restTemplate para consumir el api de productos

--- src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -1,11 +1,16 @@
 package com.formacionbdi.springboot.app.item.models.service;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -15,11 +20,17 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public List<Item> findAll() {
-        return null;
+
+        List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class));
+
+        return productos.stream().map(p-> new Item(p, 1)).collect(Collectors.toList());  //lo que hacemos es convertir la lista productos en tipo Item
     }
 
     @Override
     public Item findById(Long id, Integer cantidad) {
-        return null;
+        Map<String,String> pathVariables = new HashMap<String,String>();
+        pathVariables.put("id", id.toString());
+        Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariables);
+        return new Item(producto, cantidad);
     }
 }
